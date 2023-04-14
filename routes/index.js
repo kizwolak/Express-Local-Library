73e602e0567a0105ba32608ee95b93e39a24cef9
kizwolak <ki.zwolak@gmail.com>
2023-04-14 17:38:58 -0300

    Connect to MongoDB cluster

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,8 @@
 const express = require("express");
 const router = express.Router();
+const mongoose = require("mongoose");
+
+mongoose.set("strictQuery", false);
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
