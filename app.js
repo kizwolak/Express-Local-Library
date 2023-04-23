a55f8052ac2dedbf7037bfee532890cd26584e9e
kizwolak <ki.zwolak@gmail.com>
2023-04-23 13:36:21 -0300

    Add mongoDB functionality

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const createError = require("http-errors");
 const express = require("express");
+require("dotenv").config();
 const path = require("path");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
@@ -7,8 +8,8 @@ const mongoose = require("mongoose");
 mongoose.set("strictQuery", false);
 const indexRouter = require("./routes/index");
 const usersRouter = require("./routes/users");
-const catalogRouter = require("./routes/catalog")
-
+const catalogRouter = require("./routes/catalog");
+const mongoDB = process.env.MONGODB_SECRET;
 const app = express();
 
 // view engine setup
@@ -23,7 +24,7 @@ app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
-app.use("/catalog", catalogRouter)
+app.use("/catalog", catalogRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
