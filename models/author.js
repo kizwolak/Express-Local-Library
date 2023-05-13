37d68f8af0a7bc685a259098e44cc6159760d0a5
kizwolak <ki.zwolak@gmail.com>
2023-05-13 14:28:42 -0300

    Add author form

--- models/author.js
+++ models/author.js
@@ -10,10 +10,7 @@ const AuthorSchema = new Schema({
   date_of_death: { type: Date },
 });
 
-// Virtual for author's full name
 AuthorSchema.virtual("name").get(function () {
-  // To avoid errors in cases where an author does not have either a family name or first name
-  // We want to make sure we handle the exception by returning an empty string for that case
   let fullname = "";
   if (this.first_name && this.family_name) {
     fullname = `${this.family_name}, ${this.first_name}`;
@@ -24,9 +21,7 @@ AuthorSchema.virtual("name").get(function () {
   return fullname;
 });
 
-// Virtual for author's URL
 AuthorSchema.virtual("url").get(function () {
-  // We don't use an arrow function as we'll need the this object
   return `/catalog/author/${this._id}`;
 });
 
@@ -42,5 +37,4 @@ AuthorSchema.virtual("death_date_formatted").get(function () {
     : "";
 });
 
-// Export model
 module.exports = mongoose.model("Author", AuthorSchema);
